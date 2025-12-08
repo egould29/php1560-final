69ab582a0871788a79e0f67ae1d769af3ff9c43e
egould29 <57148464+egould29@users.noreply.github.com>
2025-12-08 11:28:18 -0500

    Finished preprocessing

--- src/main.R
+++ src/main.R
@@ -1,1 +1,10 @@
 # Main Pipeline
+library(tidyverse)
+
+source("preprocessing.R")
+
+# Read in the data
+bus_data_raw <- read_csv("../data/otp_simulated.csv")
+
+# Preprocess the data
+bus_data <- preprocess(bus_data_raw)
--- src/preprocessing.R
+++ src/preprocessing.R
@@ -1,1 +1,29 @@
 # Data Import and Cleaning
+library(dplyr)
+
+#' @description Pre-processes the RIPTA on-time data by modifying column
+#' types and dropping irrelevant and/or incomplete data.
+#' 
+#' @param data A data frame containing the raw RIPTA on-time data.
+#' 
+#' @return A correctly-formatted data frame containing the relevant data to our
+#' analysis.
+preprocess <- function(data) {
+  # Check the input
+  all_cols <- c("Date", "Driver.ID", "Route", "Trip", "Stop", "Stop.Sequence",
+                "Scheduled.Time", "Actual.Arrival.Time", "Delay.Sec",
+                "StopLat", "StopLng")
+  if (!is.data.frame(data)) {
+    stop("Preprocessing Error: Argument must be of class 'data frame'.")
+  } else if ((sum(colnames(data) %in% all_cols) != 11) |
+      (!(sum(colnames(data) %in% all_cols))) > 0) {
+    stop("Preprocessing Error: Input data does not have correct column names.")
+  }
+  
+  # Drop unused columns and filter incomplete rows
+  cleaned <- data %>%
+    select(-c(Date, StopLat, StopLng))
+  cleaned <- cleaned[complete.cases(cleaned), ]
+  
+  return(cleaned)
+}
