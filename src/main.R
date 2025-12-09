87c46b198d6ce56045207dca25bc2788c4f31ebc
egould29 <57148464+egould29@users.noreply.github.com>
2025-12-09 10:15:51 -0500

    joined data

--- src/main.R
+++ src/main.R
@@ -4,12 +4,22 @@ library(tidyverse)
 
 source("src/preprocessing.R")
 source("src/delays.R")
+source("src/routes.R")
+source("src/transitions.R")
 
-# Read in the data
+# Read in and preprocess the data
 bus_data_raw <- read_csv("data/otp_simulated.csv")
-
-# Preprocess the data
 bus_data <- preprocess(bus_data_raw)
 
-# Add the change in delays
+# Add the change in delays within each trip
 bus_delays <- calculate_delays(bus_data)
+
+# Generate route delay summaries
+route_delays <- summarize_route_delay(bus_delays)
+
+# Generate transition summary
+transitions <- identify_driver_transitions(bus_data) |>
+  summarize_transition_effects()
+
+# Join delay data into full bus data
+bus_data <- left_join(bus_delays, route_delays, by = "Route")
