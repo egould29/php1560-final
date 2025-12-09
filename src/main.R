464b1bb34afd20fd93e63ed38c454ea4b01daac6
hburtsev <hlib_burtsev@brown.edu>
2025-12-08 19:42:29 -0500

    Update main.R

--- src/main.R
+++ src/main.R
@@ -1,10 +1,11 @@
 # Main Pipeline
+# set you working directory to the php1560-final 
 library(tidyverse)
 
-source("preprocessing.R")
+source("src/preprocessing.R")
 
 # Read in the data
-bus_data_raw <- read_csv("../data/otp_simulated.csv")
+bus_data_raw <- read_csv("data/otp_simulated.csv")
 
 # Preprocess the data
 bus_data <- preprocess(bus_data_raw)
