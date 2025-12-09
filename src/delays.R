c4f84a5598677d4ef9e05475d74a693ae74f6a7f
egould29 <57148464+egould29@users.noreply.github.com>
2025-12-09 08:03:27 -0500

    finished delays, need to document

--- src/delays.R
+++ src/delays.R
@@ -1,8 +1,37 @@
 # Trip-Level Delay Analysis
 library(dplyr)
 
-source("utils.R")
+source("src/utils.R")
 
-test <- bus_data %>%
-  group_by(Date, Trip, Stop.Sequence) %>%
-  summarize(total = n())
+get_trip_delay <- function(trip_data) {
+  if (!is.data.frame(trip_data)) {
+    stop("Argument must be of type 'data frame'.")
+  }
+  
+  trip_data <- arrange(trip_data, Stop.Sequence)
+  delay_changes <- c(0)
+  
+  for (i in 1:(nrow(trip_data)-1)) {
+    change <- trip_data$Delay.Sec[i+1] - trip_data$Delay.Sec[i]
+    delay_changes <- c(delay_changes, change)
+  }
+  
+  return(delay_changes)
+}
+
+calculate_delays <- function(data) {
+  data <- data %>%
+    mutate(Delta.Delay = rep(0, nrow(data)))
+  
+  i <- 1
+  while (i <= nrow(data)) {
+    curr_trip <- get_trip(data, data$Trip[i], data$Date[i])
+    trip_length <- nrow(curr_trip)
+    
+    data$Delta.Delay[i:(i + trip_length - 1)] <- get_trip_delay(curr_trip)
+    
+    i <- i + trip_length
+  }
+  
+  return(data)
+}
--- src/main.R
+++ src/main.R
@@ -3,9 +3,13 @@
 library(tidyverse)
 
 source("src/preprocessing.R")
+source("src/delays.R")
 
 # Read in the data
 bus_data_raw <- read_csv("data/otp_simulated.csv")
 
 # Preprocess the data
 bus_data <- preprocess(bus_data_raw)
+
+# Add the change in delays
+bus_delays <- calculate_delays(bus_data)
